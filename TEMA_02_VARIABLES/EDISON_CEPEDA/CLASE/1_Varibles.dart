82416b31b7a1d74a780f9c888a84fc63d2b40d7b
CEPE1724 <132393350+CEPE1724@users.noreply.github.com>
2024-07-30 21:09:12 -0500

    cambios

--- TEMA_02_VARIABLES/EDISON_CEPEDA/CLASE/1_Varibles.dart
+++ TEMA_02_VARIABLES/EDISON_CEPEDA/CLASE/1_Varibles.dart
@@ -5,6 +5,14 @@ void main () {
   dynamic nombre2 = "Edison";
   late String edad;
   edad = "25";
+
+  print(nombre);
+  print(nombre2);
+  print(edad);
+
+  const String valorPi = "3.1416";
+  final String valorPi2 = "3.1416";
+
   
 
 }
